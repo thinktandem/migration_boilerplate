9e99cd2a3f2d32dc959683f964e0e750b54aa6b2
John Ouellet <john@thinktandem.io>
2019-02-13 18:26:07 -0500

    Add in settings

--- README.md
+++ README.md
@@ -4,4 +4,16 @@ Use this module and the directions below as a starting point to easily get your 
 
 ## SETUP
 
-1. 
+1. Setup both your Drupal 7 and Drupal 8 sites in Lando
+2. Copy the .lando.yml file in this repo to your Drupal 8 site root (change the app name)
+3. Export your Drupal 7 DB with ```lando db-export dump.sql.gz```
+4. Copy the settings.local.php file in this repo to your Drupal 8 sites/default folder.
+5. Import the Drupal 7 DB into the Drupal 8 Site with ```lando db-import --host=d7db --user=drupal7db dump.sql.gz```
+6. Run the following commands in your D8 site:
+
+```bash
+lando drush migrate-upgrade --legacy-db-key=migrate --configure-only
+lando drush config-export --destination=/tmp/migrate
+lando ssh
+cp /tmp/migrate/migrate_plus.* /app/path/to/migration_boilerplate/unused_config
+```
---
+++ settings.local.php
@@ -0,0 +1,31 @@
+<?php
+
+$databases['default']['default'] = [
+  'database' => 'drupal8',
+  'username' => 'drupal8',
+  'password' => 'drupal8',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+
+$databases['migrate']['default'] = [
+  'database' => 'drupal7db',
+  'username' => 'drupal7db',
+  'password' => 'drupal7db',
+  'prefix' => '',
+  'host' => 'd7db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+$settings['extension_discovery_scan_tests'] = FALSE;
